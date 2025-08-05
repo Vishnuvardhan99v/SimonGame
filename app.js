df04f2dfbeade54d9988900c3fc2745ed7b55d62
Vishnuvardhan99v <vishnuvardhan99v@gmail.com>
2025-08-05 23:10:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+let gameseq=[];
+let userseq=[];
+let btnarr=["red","yellow","green","purple"];
+let started =false;
+let level=0;
+
+let h2=document.querySelector("h2");
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("started");
+        started=true;
+        levelup();
+    }
+})
+
+function gameflash(btn){
+    btn.classList.add("flash");//create the class in css and adding it here
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userflash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function levelup(){
+    userseq=[];
+    level++;
+    h2.innerText=`level ${ level}`
+
+    //random button During initial flashing randmm button flashes
+    let randIdx=Math.floor(Math.random()*3)
+    let randcolor=btnarr[randIdx];
+    let randbtn=document.querySelector(`.${randcolor}`);
+    gameseq.push(randcolor);//initial adding of intial random color into seq
+    console.log(gameseq);
+    gameflash(randbtn);
+
+}
+
+let allBtns=document.querySelectorAll(".btn");
+
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+//Event Listeners
+function btnPress(){
+    let btn=this;
+    userflash(btn);
+
+    userColor=btn.getAttribute('id');//getting id of button pressed
+    userseq.push(userColor);
+    checkAns(userseq.length-1);
+}
+
+function checkAns(idx){
+    
+    //console.log("curr level ",level);
+    if(userseq[idx]===gameseq[idx]){
+        if(userseq.length==gameseq.length){
+            setTimeout(levelup,1000);
+        }
+     
+    }else{
+        h2.innerHTML=`Game Over! your score is <b>${level*10}<b> <br> press any key to start`;
+        document.querySelector('body').style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector('body').style.backgroundColor="white";
+        },150);
+        reset();
+    }
+
+}
+
+function  reset(){
+    started=false;
+    gameseq=[];
+    userseq=[];
+    level=0;
+}
+
+
+
